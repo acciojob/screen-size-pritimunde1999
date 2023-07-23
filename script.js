d4b7098506868e0a68f199780332f310d9be6998
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-23 16:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function resizeWindow(){
+    const width = window.innerWidth;
+    const heigth = window.innerHeight;
+
+    const div = document.getElementById("sizeInfo");
+
+    div.innerHTML = `<h1>Width: ${width} and Height: ${heigth}`
+}
+
+ window.addEventListener("resize",resizeWindow);
+
+ resizeWindow();
